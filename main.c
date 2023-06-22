72ce86f0598bc595d9bd2e824894745d0956a004
elif7707 <elifc6803@gmail.com>
2023-06-22 16:47:06 +0300

    created summation function

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+
+int findSum(int num1, int num2) {
+    return num1 + num2;
+}
+
 int main() {    
 
     int number1, number2, sum;
@@ -7,7 +12,7 @@ int main() {
     scanf("%d %d", &number1, &number2);
 
     // calculate the sum
-    sum = number1 + number2;      
+    sum = findSum(number1, number2);     
     
     printf("%d + %d = %d", number1, number2, sum);
     return 0;
